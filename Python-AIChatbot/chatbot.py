53acef0a8f2771cfa681ce99b2fd3dba8320d8a9
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-04-29 22:03:27 -0400

    Bot remembers previous conversations

--- Python-AIChatbot/chatbot.py
+++ Python-AIChatbot/chatbot.py
@@ -6,19 +6,21 @@ load_dotenv()
 
 # Set the OpenAI API key
 openai.api_key = os.getenv("YOUR_OPENAI_API_KEY")
+
 #  Define the function to generate the response from the AI
-def generate_response(prompt):
+def generate_response(prompt, conversation_history):
+    prompt_with_history = f"{conversation_history}\n{prompt}"
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=prompt,
+        prompt=prompt_with_history,
         max_tokens=int(os.getenv("YOUR_DESIRED_MAX_TOKEN_AMOUNT")),
         n=int(os.getenv("NUMBER_OF_COMPLETIONS")),
         stop=None,
         temperature=float(os.getenv("YOUR_DESIRED_TEMP")),
     )
-
     message = response.choices[0].text.strip()
     return message
+
 
 # Create a file to store the conversation
 conversation_file = "conversation.txt"
@@ -32,19 +34,22 @@ if not os.path.exists(conversation_file):
 
 # Start the conversation
 print("AI: Hello, I am an AI chatbot. I am very smart. Ask me anything! If you want to stop talking to me, just type 'exit'.")
+
+conversation_history = ""
 while True:
     user_input = input("You: ")
     # Exit when the user says "exit"
     if user_input.lower() == "exit":
+        with open(conversation_file, "w") as f:
+            f.write("")
         break
     prompt = f"You say: {user_input}\nAI says:"
-    response = generate_response(prompt)
+    response = generate_response(prompt, conversation_history)
     print("AI:", response)
 
     with open(conversation_file, "a") as f:
         f.write(user_input + "\n")
         f.write(response + "\n")
 
-# Clear the conversation file
-with open(conversation_file, "w") as f:
-    f.write("")
+    # Update conversation history
+    conversation_history += f"\nYou say: {user_input}\nAI says: {response}"
