2927c41f95e36542a9cd86ba97ce712dc37d8bcd
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-04-29 21:49:26 -0400

    Added exit functionality and .env example

--- Python-AIChatbot/chatbot.py
+++ Python-AIChatbot/chatbot.py
@@ -4,20 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Set the OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("YOUR_OPENAI_API_KEY")
 
-# Define the function to generate the response
+#  Define the function to generate the response from the AI
 def generate_response(prompt):
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    max_tokens=os.getenv("YOUR_DESIRED_MAX_TOKEN_AMOUNT"),
-    temperature=os.getenv("YOUR_DESIRED_TEMP"),
-    messages = response.choices[0].text.strip()
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=int(os.getenv("YOUR_DESIRED_MAX_TOKEN_AMOUNT")),
+        n=int(os.getenv("NUMBER_OF_COMPLETIONS")),
+        stop=None,
+        temperature=float(os.getenv("YOUR_DESIRED_TEMP")),
     )
-    return response
 
-# Define the file to store the conversation
+    message = response.choices[0].text.strip()
+    return message
+
+# Create a file to store the conversation
 conversation_file = "conversation.txt"
 
 # Create the file if it doesn't exist
@@ -25,9 +28,13 @@ if not os.path.exists(conversation_file):
     with open(conversation_file, "w") as f:
         f.write("")
 
-# Open the file and read the last line
+# Start the conversation
+print("AI: Hello, I am an AI chatbot. I am very smart. Ask me anything! If you want to stop talking to me, just type 'exit'.")
 while True:
     user_input = input("You: ")
+    # Exit when the user says "exit"
+    if user_input.lower() == "exit":
+        break
     prompt = f"You say: {user_input}\nAI says:"
     response = generate_response(prompt)
     print("AI:", response)
